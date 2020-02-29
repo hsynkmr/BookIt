ece4cb7c0d53a6c56bec466ec712b61c35e44b44
hsynkmr <57600552+hsynkmr@users.noreply.github.com>
2020-02-29 15:48:20 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,7 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Hello hello");
+        
+         System.out.println("Good bye Git") // this line is from Github
     }
 }
