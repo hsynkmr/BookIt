d2444c6c160d213ba846a2e09dcb6c859a9990a3
HsynKmr <huseyinkumru89@gmail.com>
2020-02-29 15:58:16 -0500

    update2 LocatingMap.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -4,6 +4,6 @@ public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Hello hello");
         
-         System.out.println("Good bye Git") // this line is from Github
+         System.out.println("Good bye Git"); // this line is from Github
     }
 }
--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -1,8 +1,11 @@
 package test;
 
+import java.sql.SQLOutput;
+
 public class LocatingMap {
     public static void main(String[] args) {
         //
 
+        System.out.println("We are about to do conflicts");
     }
 }
